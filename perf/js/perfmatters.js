dff675275bd699b1517b963cd66cf625e0cef2ca
Shahrukh Syed <ss153g@att.com>
2015-01-11 11:23:59 -0600

    minified css, js and combined js

--- perf/js/perfmatters.js
+++ perf/js/perfmatters.js
@@ -12,3 +12,4 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+(function(w,g){w['GoogleAnalyticsObject']=g;w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');ga('create', 'UA-XXXX-Y');ga('send', 'pageview');
